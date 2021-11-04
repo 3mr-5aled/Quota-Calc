b9c9d41fbff3235bf4552ff7b669d37ae0426186
Amr Khaled <moroamr2005@gmail.com>
2021-11-04 18:11:11 +0200

    fixes the nan problem

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-    // Today function (Still Not working)
-    // var today = new Date();
-    // let myDate1 = today.toLocaleDateString();
-    // myDate1 = myDate1.split("/");
-    // var date1 = new Date(myDate1[1], myDate1[0] - 1, myDate1[2]);
-    // console.log(date1.getTime());
-    // let date1 = new Date("08/16/2021");
-    // let date2 = new Date("09/03/2021");
     var btnGet = document.querySelector("button");
     var result = document.querySelector("h3");
     
@@ -48,16 +40,16 @@
 
         // 🐞bug : it runs even if all required data is entered
 
-        // if (isNaN(num)) {
-        //   document.write("<div><h1>❗❗ Make sure you have entered all required data ❗❗</h1><button onClick=window.location.reload()>Back To calc</button></div>")
-        //   document.querySelector("body").style = "background:linear-gradient(180deg,rgb(50,1,65),rgb(184,0,190,1));color:white;justify-content:center;display:flex;text-align:center;align-items:center;flex-wrap:wrap;";
-        //   document.querySelector("button").style= "margin:10px;font-size:24px;color:white;background:transparent;border:5px solid rgb(204, 0, 204);border-radius: 5px;";
-        //   document.querySelector("button").onmouseover = function() {
-        //             this.style= "background-color: rgb(50,1,65);border: 5px solid rgb(50,1,65);margin:10px;font-size:24px;color:white;border-radius: 5px;";
-        //         }
-        // document.querySelector("button").onmouseout = function() {
-        //     this.style= "margin:10px;font-size:24px;color:white;background:transparent;border:5px solid rgb(204, 0, 204);border-radius: 5px;";
-        // }
-        // }
+        if (isNaN(average)) {
+          document.write("<div><h1>❗❗ Make sure you have entered all required data ❗❗</h1><button onClick=window.location.reload()>Back To calc</button></div>")
+          document.querySelector("body").style = "background:linear-gradient(180deg,rgb(50,1,65),rgb(184,0,190,1));color:white;justify-content:center;display:flex;text-align:center;align-items:center;flex-wrap:wrap;";
+          document.querySelector("button").style= "margin:10px;font-size:24px;color:white;background:transparent;border:5px solid rgb(204, 0, 204);border-radius: 5px;";
+          document.querySelector("button").onmouseover = function() {
+                    this.style= "background-color: rgb(50,1,65);border: 5px solid rgb(50,1,65);margin:10px;font-size:24px;color:white;border-radius: 5px;";
+                }
+        document.querySelector("button").onmouseout = function() {
+            this.style= "margin:10px;font-size:24px;color:white;background:transparent;border:5px solid rgb(204, 0, 204);border-radius: 5px;";
+        }
+    }
     });
     
