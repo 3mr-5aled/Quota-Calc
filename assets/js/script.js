ec3bba390fd9d64d0feb50bd7ad16ef5ed4a9934
Amr Khaled <moroamr2005@gmail.com>
2022-06-05 21:54:29 +0200

    fixed margins, back to calc place

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var btnGet = document.getElementById("btc");
 var result = document.querySelector("h3");
 
-document.getElementById("btc2").style.display = "none";
+document.getElementById("bth").style.display = "none";
 
 btnGet.addEventListener("click", () => {
   var myDate1 = document.getElementById("startDate").value;
@@ -30,16 +30,10 @@ btnGet.addEventListener("click", () => {
   num.innerHTML = average;
 
   // organizing displays
-  document.querySelector("form").style.display = "none";
-  document.getElementById("btc").style.display = "none";
-  document.querySelector("h4").style.display = "block";
-  document.querySelector("h3").style.display = "block";
-  document.getElementById("btc2").style.display = "block";
-
-  // if (window.matchMedia("(max-width: 600px)").matches) {
-  //   document.querySelector("body > header > div").style.flexDirection =
-  //     "column";
-  // }
+  document.querySelector(".right").style.display = "none";
+  document.getElementById("bth").style.display = "block";
+  // document.getElementById("btc").style.display = "none";
+  // document.querySelector(".left").style.order = "1";
 
   // NAN Solution
 
@@ -47,7 +41,7 @@ btnGet.addEventListener("click", () => {
 
   if (isNaN(average)) {
     document.write(
-      "<div><h1>❗❗ Make sure you have entered all required data ❗❗</h1><button onClick=window.location.reload()>Back To calc</button></div>"
+      "<div><h1>❗❗ Make sure you have entered all required data correctly ❗❗</h1><button onClick=window.location.reload()>Back To calc</button></div>"
     );
     document.querySelector("body").style =
       "background:linear-gradient(180deg,rgb(50,1,65),rgb(184,0,190,1));color:white;justify-content:center;display:flex;text-align:center;align-items:center;flex-wrap:wrap;";
@@ -82,3 +76,40 @@ btnGet.addEventListener("click", () => {
 //       loader.style.display = "none";
 //     }, 1500);
 //   });
+
+// make enter button calculate
+
+var input = document.getElementById("GigaB");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btc").click();
+  }
+});
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
