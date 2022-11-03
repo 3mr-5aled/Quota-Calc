d0d07def0f1a6b3c7010f1d2b9381d37d20b7c02
Amr Khaled <moroamr2005@gmail.com>
2022-11-03 19:00:54 +0200

    sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,18 +1,3 @@
-// This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+precacheAndRoute([{ "revision": "edfcd6d6702e03365f04d5df00e463e1", "url": "404.html" }, { "revision": "2956ff41a5b6ae36bca5b4b83856a222", "url": "index-ar.html" }, { "revision": "358e0f14955896166c82a958f60b49be", "url": "index.html" }, { "revision": "c55a1e6ab85988e33de20ee6806eb72c", "url": "pwabuilder-sw.js" }, { "revision": "56b6e7bc19feadad49cf32a46965d8c4", "url": "src/pages/contact-ar.html" }, { "revision": "4f36029542ed0a929058622a0cc032d6", "url": "src/pages/contact.html" }, { "revision": "86dfbac6ff69220066505d83fd1664e8", "url": "src/pages/tips-ar.html" }, { "revision": "c9f10e0d0663814502d58432eb1098ec", "url": "src/pages/tips.html" }, { "revision": "0b483751275ccada17d513109a6c4705", "url": "src/script/script.js" }, { "revision": "50ba176d52b4d5955d9b483388f05b55", "url": "src/script/scroll-to-top.js" }, { "revision": "a071a4c1810df90775c0ac7ed14b7853", "url": "src/styles/contact-ar.css" }, { "revision": "176f1a9e30458a34523f066d7f60d73f", "url": "src/styles/contact.css" }, { "revision": "c0a9f399cc209086e3405085d442a154", "url": "src/styles/font-awesome-animation.min.css" }, { "revision": "a891a31af2caeedc53002f5043752045", "url": "src/styles/main-ar.css" }, { "revision": "540d907efd58655fc3513f8a3d647378", "url": "src/styles/main.css" }, { "revision": "3e656042033db27bd99300dcecbe3e59", "url": "src/styles/style-ar.css" }, { "revision": "c5cef938284d265351964be3accd362a", "url": "src/styles/style.css" }, { "revision": "5721986f162d12664ea4a87f8678844c", "url": "src/styles/tips.css" }]);
