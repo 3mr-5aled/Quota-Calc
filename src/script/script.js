2435bc011527f196f9a46277962affe8a63f0189
3mr-5aled <moroamr2005@gmail.com>
2022-11-11 00:01:36 +0200

    popup message and toggle message

--- src/script/script.js
+++ src/script/script.js
@@ -97,3 +97,42 @@ input.addEventListener("keypress", function (event) {
     document.getElementById("btc").click();
   }
 })
+
+
+
+// tabs in pop up card
+
+function switchTab(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// pop up card
+// document.getElementsById('btc2').addEventListener('click', function () {
+//   document.querySelector('.pop-up').style.display = 'flex';
+// })
+document.getElementById('close').addEventListener('click', function () {
+  document.querySelector('.pop-up').style.display = 'none';
+})
+document.getElementById('btc2').addEventListener('click', function () {
+  document.querySelector('.pop-up').style.display = 'flex';
+})
+document.getElementById('btc3').addEventListener('click', function () {
+  document.querySelector('.pop-up').style.display = 'flex';
+})
